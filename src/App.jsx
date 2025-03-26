6045c1fa98177de39d3acf7e9c2a2a8e6b2d9238
neel <neelgupta701@gmail.com>
2025-03-26 18:50:29 +0530

    add first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './assets/style.scss';
-// import VendorOnboarding from './VendorOnboarding';
+import './assets/style.scss';
+import VendorOnboarding from './VendorOnboarding';
 
 const App = () => {
   return (
     <>
-      {/* <VendorOnboarding/> */}
-      OK
+      <VendorOnboarding/>
     </>
   )
 }
